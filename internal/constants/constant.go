31852d42964251067a69747a29deab1791ba78b9
birdhk <moon0hello@gmail.com>
2023-11-16 15:49:02 +0800

    fix bug

--- internal/constants/constant.go
+++ internal/constants/constant.go
@@ -4,6 +4,6 @@ const (
 	SecretLabelKey  = "app.kubernetes.io/component"
 	Aggregator      = "Aggregator"
 	Agent           = "Agent"
-	FilePath        = "./"
+	FilePath        = "/etc/vector"
 	SecretFinalizer = "finalizers.kubesphere.io/secret-finalizer"
 )
